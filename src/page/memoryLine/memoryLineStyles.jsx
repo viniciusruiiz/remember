f92cda678a1042e6d8da33ea88efbb74f90dc4b2
Gabriela Delfino <gabriela.delfino@outlook.com.br>
2019-10-01 19:11:26 -0300

    resize lineboxes

---
+++ src/page/memoryLine/memoryLineStyles.jsx
@@ -0,0 +1,10 @@
+import { createStyles } from "@material-ui/core";
+
+const styles = (theme) => createStyles({
+    root: {
+        marginTop: 56,
+        color: 'white',
+    },
+});
+
+export default styles
