1eb40f94c17f3a032f8fb9fdaec0c561ebb1e0f8
vsruiz <vsruiz@stefanini.com>
2019-10-03 14:59:12 -0300

    .

--- src/model/response/preSignedUrlResponse.ts
+++ src/model/response/preSignedUrlResponse.ts
@@ -2,4 +2,5 @@ import BaseResponse from "../baseResponse";
 
 export default class PreSignedUrlResponse extends BaseResponse<PreSignedUrlResponse> {
     presigned_url: string;
+    mime_type: string
 }
