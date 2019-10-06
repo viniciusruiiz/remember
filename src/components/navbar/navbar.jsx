1172db98847b92928f57725b19a634d216cd6436
Lucas Yudi Ganeko <lucas.yg2@gmail.com>
2019-10-06 01:47:57 -0300

    menu fix

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppBar, Toolbar, IconButton, Typography, withStyles, Button, Menu, MenuItem } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, withStyles, Button, MenuItem, Paper, MenuList, ClickAwayListener } from '@material-ui/core';
 import styles from './navbarStyles';
 import logo from './../../images/logo-icon.png';
 import perfil from './../../images/perfil.jpg';
@@ -9,17 +9,21 @@ class NavBar extends Component {
         super(props)
         this.state = {
             anchorEl: null,
-            setAnchorEl: null,
+            open: false
         }
     }
 
     handleClick = (event) => {
         this.setState({anchorEl: event.currentTarget})
-        console.log('oi')
+        this.setState({open: !this.state.open})
     };
 
     handleClose = () => {
         this.setState({anchorEl: null})
+    };
+
+    handleClickAway = () => {
+        this.setState({open: false})
     };
 
     render() {
@@ -30,22 +34,22 @@ class NavBar extends Component {
          <div>
             <AppBar className={classes.bar} position="fixed">
                 <Toolbar variant="dense">
-                    <img src={logo} className={classes.logoIcon}/>
+                    <img alt='' src={logo} className={classes.logoIcon}/>
                     <Typography className={classes.logoText}>remember</Typography>
+                    <ClickAwayListener onClickAway={this.handleClickAway}>
                     <Button className={classes.button} aria-controls="simple-menu" aria-haspopup="true" onClick={this.handleClick}>
                         <Typography className={classes.user}>Lucas</Typography>
-                        <img src={perfil} className={classes.perfil}/>
+                        <img alt='' src={perfil} className={classes.perfil}/>
+                        {this.state.open && 
+                        <Paper className={classes.paper}>
+                            <MenuList>
+                                <MenuItem onClick={this.handleClose}>Minha conta</MenuItem>
+                                <MenuItem onClick={this.handleClose}>Sair</MenuItem>
+                            </MenuList>
+                        </Paper>
+                        }
                     </Button>
-                    <Menu
-                        id="simple-menu"
-                        anchorEl={this.state.anchorEl}
-                        keepMounted
-                        open={Boolean(this.state.anchorEl)}
-                        onClose={this.handleClose}
-                    >
-                        <MenuItem onClick={this.handleClose}>Minha conta</MenuItem>
-                        <MenuItem onClick={this.handleClose}>Sair</MenuItem>
-                    </Menu>
+                    </ClickAwayListener>
                 </Toolbar>
             </AppBar>
         </div>
--- src/components/navbar/navbarStyles.jsx
+++ src/components/navbar/navbarStyles.jsx
@@ -1,4 +1,5 @@
 import { createStyles } from "@material-ui/core";
+import { grey } from "@material-ui/core/colors";
 
 const styles = (theme) => createStyles({
     bar: {
@@ -20,7 +21,13 @@ const styles = (theme) => createStyles({
         lineHeight: "100%",
         flexGrow: 1,
     },
-    user: {
+    paper: {
+        zIndex: 2,
+        position: 'absolute',
+        top: 56,
+        width: 200,
+        right: 0,
+        left: 0,
     },
     button: {
         padding: theme.spacing(0,1,0,2),
