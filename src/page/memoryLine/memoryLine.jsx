a3ea38902ec726bde17c3a2ce01fa443fee917c5
vsruiz <vsruiz@stefanini.com>
2019-10-10 15:22:20 -0300

    comecando drag and drop

--- src/page/memoryLine/memoryLine.jsx
+++ src/page/memoryLine/memoryLine.jsx
@@ -6,24 +6,40 @@ import Line from '../../components/line/line';
 import MomentService from '../../service/momentService';
 import { Fab, Typography } from '@material-ui/core';
 import { Add, NavigateBefore } from '@material-ui/icons';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 class MemoryLine extends Component {
-    
+
     _ms = new MomentService()
     _queryString;
 
     constructor(props) {
         super(props)
         this.state = {
-            moments: []
+            moments: [],
+            openModal: false
         }
 
         this._queryString = new URLSearchParams(this.props.location.search)
-        
+
         this._ms.getAllMoments(this._queryString.get("ref")).then(res => {
-            this.setState({"moments": res.data.data})
+            this.setState({ "moments": res.data.data })
         })
     }
+
+    handleClickOpen = () => {
+        this.setState({ "openModal": true })
+    };
+
+    handleClose = () => {
+        this.setState({ "openModal": false })
+    };
 
     // handleFile = (e) => {
     //     this.setState({'file':e.target.files[0]})
@@ -51,19 +67,60 @@ class MemoryLine extends Component {
         document.title = this._queryString.get("title") // passar o nome da memory line
 
         return (
-        <>
-            <NavBar />
-            <div className={classes.root}>
-                <Typography className={classes.title}>
-                    <NavigateBefore className={classes.back}/> 
-                    { this._queryString.get("title") || 'Memoryline Title' }
-                </Typography>
-                <Line data={this.state.moments} />
-                <Fab color="primary" aria-label="add" className={classes.fab} >
-                    <Add />
-                </Fab>
-            </div>
-        </>
+            <>
+                <NavBar />
+
+                <div className={classes.root}>
+                    <Typography className={classes.title}>
+                        <NavigateBefore className={classes.back} />
+                        {this._queryString.get("title") || 'Memoryline Title'}
+                    </Typography>
+                    <Line data={this.state.moments} />
+
+                    <Fab color="primary" aria-label="add" className={classes.fab} onClick={this.handleClickOpen} >
+                        <Add />
+                    </Fab>
+                </div>
+
+                {/* MODAL ADD MOMENT */}
+                <Dialog open={this.state.openModal} onClose={this.handleClose} aria-labelledby="form-dialog-title">
+                    <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>
+                            To subscribe to this website, please enter your email address here. We will send updates
+                            occasionally.
+                </DialogContentText>
+                        {/* <TextField
+                            autoFocus
+                            margin="dense"
+                            id="name"
+                            label="Email Address"
+                            type="email"
+                            fullWidth
+                        /> */}
+                        <input
+                            accept="image/*"
+                            style={{ display: 'block' }}
+                            id="raised-button-file"
+                            multiple
+                            type="file"
+                        />
+                        <label htmlFor="raised-button-file">
+                            <Button variant="raised" component="span">
+                                Upload
+  </Button>
+                        </label>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClose} color="primary">
+                            Cancel
+                </Button>
+                        <Button onClick={this.handleClose} color="primary">
+                            Subscribe
+                </Button>
+                    </DialogActions>
+                </Dialog>
+            </>
         )
     }
 }
