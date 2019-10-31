907da479446ee4c6f410e23919faf1ad1dcff4ba
Gabriela Delfino <gabriela.delfino@outlook.com.br>
2019-10-30 21:23:09 -0300

    private and public routes

---
+++ src/service/access/PublicRoute.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import BaseService from '../baseService';
+
+const PublicRoute = ({component: Component, restricted, ...rest}) => {
+    return (
+        <Route {...rest} render={props => (
+            BaseService.isAuthenticated && restricted ?
+                <Redirect to="/userhome" />
+            : <Component {...props} />
+        )} />
+    );
+};
+
+export default PublicRoute;
